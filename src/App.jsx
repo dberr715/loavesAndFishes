a1a4dc9b62e1180f6026d513694a7aeb12045adb
David Berry <dberry3755@gmail.com>
2024-11-11 11:27:40 -0500

    Revert "useless changes"
    This reverts commit 979fb2619d083c803a91c749ac4261c91ff176d4.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 import AdminManagement from "./components/AdminManagement.jsx";
+
 import RouteManagement from "./components/RouteManagement.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/route-management">
-                Route Management
-              </Link>
+            <li>
+              <Link to="/route-management">Route Management</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/admin-management">
-                Admin Management
-              </Link>
+            <li>
+              <Link to="/admin-management">Admin Management</Link>
             </li>
           </ul>
         </nav>
